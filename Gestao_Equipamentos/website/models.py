6151996e70e539d1a329ec952e979a9da69800db
Kalumana <estgv18177@alunos.estgv.ipv.pt>
2023-11-02 11:11:53 +0000

    update  models for db tables

--- Gestao_Equipamentos/website/models.py
+++ Gestao_Equipamentos/website/models.py
@@ -25,19 +25,37 @@ class Clients(AbstractUser):
 class Component(models.Model):
     id_comp = models.BigAutoField(primary_key=True, default=int)
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField()  
+    price = models.DecimalField(decimal_places=2, max_digits=6)
 
-class Equipment(models.Model):
+class Types_of_Labor(models.Model):
+    id_labor = models.BigAutoField(primary_key=True, default=int)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+
+    
+class ProdEquipment(models.Model):
     id_equip = models.BigAutoField(primary_key=True, default=int) 
     name = models.CharField(max_length=100)
     description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=6)
     components = models.ManyToManyField(Component, through='ComponentUsage')
+    type_equip = models.CharField(max_length=100)
+    type_labor = models.ManyToOneRel(Types_of_Labor, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    state = models.CharField(choices=(
+          ('1', "producing"),
+          ('2', "produced"),
+          ),
+          max_length=1,
+          default='producing'
+          )
 
 class ComponentUsage(models.Model):
-    id_comp_usage = models.BigAutoField(primary_key=True, default=int)
     component = models.ForeignKey(Component, on_delete=models.CASCADE)
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    equipment = models.ForeignKey(ProdEquipment, on_delete=models.CASCADE)
+   
 
 class StockComponents(models.Model):
     id_stock_comp = models.BigAutoField(primary_key=True, default=int)
@@ -47,7 +65,7 @@ class StockComponents(models.Model):
 
 class StockEquipment(models.Model):
     id_stock_equip = models.BigAutoField(primary_key=True, default=int)
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(ProdEquipment, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     # Add other fields as needed
 
