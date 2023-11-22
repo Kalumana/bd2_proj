7007c4aab0e994fecedc89623bed72d8bd6d36c5
Kalumana <estgv18177@alunos.estgv.ipv.pt>
2023-11-22 18:07:49 +0000

    update models and admin file

--- Gestao_Equipamentos/website/admin.py
+++ Gestao_Equipamentos/website/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import ManagerProfile, ClientProfile, SupplierProfile
 
-
-admin.site.register(ManagerProfile)
-admin.site.register(SupplierProfile)
-admin.site.register(ClientProfile)
-
-
-
-
-
-
-
+# Register your models here.
--- Gestao_Equipamentos/website/models.py
+++ Gestao_Equipamentos/website/models.py
@@ -1,14 +1,15 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
 
     
-class ManagerProfile(AbstractUser):
+class Mana(AbstractUser):
     # Add other fields for user roles and profile
     def __str__(self):
         return self.username
 
-class ClientProfile(models.Model):
+class Client(models.Model):
     id = models.BigAutoField(primary_key=True, serialize=True, default=int)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -16,26 +17,30 @@ class ClientProfile(models.Model):
     phone = models.IntegerField()
     nif = models.IntegerField()
     address = models.TextField()
+    city = models.CharField(max_length=256)
+    postal_code = models.CharField(max_length=10)
+    create_at = models.DateTimeField()
 
 
-class SupplierProfile(AbstractUser):
+class Supplier(models.Model):
    id = models.BigAutoField(primary_key=True, serialize=True, default=int)
    first_name = models.CharField(max_length=50)
    last_name = models.CharField(max_length=50)
    email = models.EmailField()
    phone = models.IntegerField()
+   nif = models.IntegerField()
    address = models.TextField()
 
 class Component(models.Model): 
-    id_comp = models.BigAutoField(primary_key=True, default=int)
+    id_comp = models.BigAutoField(primary_key=True,serialize = True, default=int)
     name = models.CharField(max_length=100)
     description = models.TextField()  
     price = models.DecimalField(decimal_places=2, max_digits=6)
     images = ArrayField(models.CharField(max_length=100, blank=True), default=list)
-    supplier = models.ForeignKey(SupplierProfile, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
 
-class Types_of_Labor(models.Model):
-    id_labor = models.BigAutoField(primary_key=True, default=int)
+class Labor(models.Model):
+    id_labor = models.BigAutoField(primary_key=True,serialize = True, default=int)
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=6)
@@ -47,20 +52,20 @@ class Equipment(models.Model):
    PRODUCED = 'produced'
    DELIVERY = 'delivery'
    STATUS = [
-       (ANALYSING, ('Analisando equipamento')),
-       (PRODUCING, ('produxindo equipamento')),
-       (PRODUCED , ('euipamento produzido')),
-       (DELIVERY , ('euipamento entregue')),
+       (ANALYSING, ('Em analise')),
+       (PRODUCING, ('A produzir')),
+       (PRODUCED , ('Produzido')),
+       (DELIVERY , ('Entregue')),
    ]
-   id_equip = models.BigAutoField(primary_key=True, default=int) 
+   id_equip = models.BigAutoField(primary_key=True, serialize = True, default=int) 
    name = models.CharField(max_length=100)
    description = models.TextField()
    price = models.DecimalField(decimal_places=2, max_digits=6)
    components = models.ManyToManyField(Component, through='ComponentUsage')
    created_at = models.DateTimeField(auto_now_add=True)
    type_equip = models.CharField(max_length=100)
-   images = ArrayField(models.ImageField(upload_to="/comp_image"))
-   type_labor = models.ForeignKey(Types_of_Labor,on_delete=models.CASCADE)
+   images = ArrayField(models.ImageField())
+   type_labor = models.ForeignKey(Labor,on_delete=models.CASCADE)
    quantity = models.PositiveIntegerField()
    state = models.CharField(
         choices= STATUS,
@@ -75,22 +80,22 @@ class ComponentUsage(models.Model):
    
 
 class StockComponents(models.Model):
-    id_stock_comp = models.BigAutoField(primary_key=True, default=int)
+    id_stock_comp = models.BigAutoField(primary_key=True,serialize = True, default=int)
     component = models.ForeignKey(Component, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
-    manager = models.OneToOneField(ManagerProfile, on_delete=models.CASCADE)
+    manager = models.OneToOneField(Mana, on_delete=models.CASCADE)
     # Add other fields as needed
 
 class StockEquipments(models.Model):
-    id_stock_equip = models.BigAutoField(primary_key=True, default=int)
+    id_stock_equip = models.BigAutoField(primary_key=True, serialize = True,default=int)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
-    manager = models.OneToOneField(ManagerProfile, on_delete=models.CASCADE)
+    manager = models.OneToOneField(Mana, on_delete=models.CASCADE)
     # Add other fields as needed
 
 class PurshaseEquipment(models.Model):
-    id_purchase = models.BigAutoField(primary_key=True, default=int)
-    user = models.ForeignKey(ClientProfile, on_delete=models.CASCADE)
+    id_purchase = models.BigAutoField(primary_key=True, serialize = True, default=int)
+    user = models.ForeignKey(Client, on_delete=models.CASCADE)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
 
