e72ddf52c8f4f21e4a041b017908eb3b50beed71
Kalumana <estgv18177@alunos.estgv.ipv.pt>
2023-11-22 18:07:21 +0000

    update settings

--- Gestao_Equipamentos/Gestao_Equipamentos/settings.py
+++ Gestao_Equipamentos/Gestao_Equipamentos/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'website.ManagerProfile'
+AUTH_USER_MODEL = 'website.Manager'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
